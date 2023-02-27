620ba87f7b0d2f7dfa7cbee99901324dc0a3a001
fabian-emmanuel <emmanuel.fabian.dev@gmail.com>
2023-02-27 20:17:06 +0100

    WIP - Ussd Logic Implementation

--- src/main/java/com/credo/ussd/service/ussd/UssdServiceImpl.java
+++ src/main/java/com/credo/ussd/service/ussd/UssdServiceImpl.java
@@ -1,60 +1,117 @@
 package com.credo.ussd.service.ussd;
 
 import com.credo.ussd.payloads.UssdRequest;
-import com.credo.ussd.payloads.UssdResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UssdServiceImpl implements UssdService {
-    private static final int STATE_MAIN_MENU = 0;
-    private static final int STATE_CREATE_ACCOUNT = 1;
-    private static final int STATE_DEPOSIT_AMOUNT = 2;
-    private static final int STATE_WITHDRAW_AMOUNT = 3;
-    private static final int STATE_CHECK_BALANCE = 4;
+import java.util.HashMap;
+import java.util.Map;
 
-    private int currentState = STATE_MAIN_MENU;
-    private String accountNumber;
-    private double balance = 0.0;
-    private String name;
-    private String email;
-    private String phone;
+@Service
+@RequiredArgsConstructor
+public class UssdServiceImpl implements UssdService {
+    // Map to store user sessions
+    private final Map<String, String[]> sessions = new HashMap<>();
 
     @Override
-    public String handleRequest(UssdRequest request) {
-        String input = request.getText().trim();
-        String response = "";
-        String sessionId = request.getSessionId();
+    public String handleRequest(UssdRequest ussdRequest) {
 
-        if (input.equals("*123#")){
-            return "Select an option:\n1. Create account\n2. Deposit\n3. Withdraw\n4. Check balance";
+        String text = ussdRequest.getText();
+        String sessionId = ussdRequest.getSessionId();
+        String serviceCode = ussdRequest.getServiceCode();
+//        String phoneNumber = ussdRequest.getMsisdn();
+//        String networkCode = ussdRequest.getNetworkCode();
+        // Get or create session data
+        String[] sessionData = sessions.getOrDefault(sessionId, new String[]{"", ""});
+        String menuLevel = sessionData[0];
+        String lastResponse = sessionData[1];
+
+        // Handle first menu screen
+        if (text.isEmpty()) {
+            String response = "CON Hi welcome, I can help you with Ussd Services\n";
+            response += "1. Enter 1 to continue";
+            lastResponse = response;
+            menuLevel = "1";
         }
 
-        return switch (input) {
-            case "1" -> createAccount(sessionId);
-            case "2" -> deposit(sessionId);
-            case "3" -> withdraw(sessionId);
-            case "4" -> checkBalance(sessionId);
-            default -> "Invalid input. Please try again.";
-        };
-    }
+        // Handle second menu screen
+        else if (menuLevel.equals("1") && text.equals("1")) {
+            String response = "CON Pick a service below\n";
+            response += "1. Create Account \n";
+            response += "2. Deposit \n";
+            response += "3. Withdraw \n";
+            response += "4. Check Balance";
+            lastResponse = response;
+            menuLevel = "2";
+        }
 
-    public String createAccount(String sessionId) {
-        // TODO: Implement logic for creating an account
-        return "Account created successfully.";
-    }
+        // Handle service selection
+        else if (menuLevel.equals("2") && text.startsWith("1")) {
+            // Create Account
+            switch (text) {
+                case "1" -> {
+                    String response = "CON You are about to Create An Account\n";
+                    response += "Please enter 1 to confirm";
+                    lastResponse = response;
+                    menuLevel = "3_1";
+                }
 
-    public String deposit(String sessionId) {
-        // TODO: Implement logic for depositing funds
-        return "Funds deposited successfully.";
-    }
+                // Deposit
+                case "2" -> {
+                    String response = "CON You are about to deposit\n";
+                    response += "Please enter 1 to confirm";
+                    lastResponse = response;
+                    menuLevel = "3_2";
+                }
 
-    public String withdraw(String sessionId) {
-        // TODO: Implement logic for withdrawing funds
-        return "Funds withdrawn successfully.";
-    }
+                // Withdraw
+                case "3" -> {
+                    String response = "CON You are about to withdraw\n";
+                    response += "Please enter 1 to confirm";
+                    lastResponse = response;
+                    menuLevel = "3_3";
+                }
 
-    public String checkBalance(String sessionId) {
-        // TODO: Implement logic for checking account balance
-        return "Your balance is $100.00";
+                // Check Balance
+                case "4" -> {
+                    String response = "CON You are about to check balance\n";
+                    response += "Please enter 1 to confirm";
+                    lastResponse = response;
+                    menuLevel = "3_4";
+                }
+            }
+        }
+
+        //Handle Service Logic
+        else if (menuLevel.startsWith("3")) {
+            switch (menuLevel) {
+                case "3_1" -> {
+                    String response = "CON Enter your first name";
+                    response += "Enter your last name";
+                    response += "Enter your phone number";
+                    response += "Enter your email";
+                    response += "Enter a 4-digit pin";
+                    lastResponse = response;
+                    menuLevel = "4_1";
+                }
+                case "3_2" -> {
+                    String response = "CON Enter amount to deposit";
+                    lastResponse = response;
+                    menuLevel = "4_2";
+                }
+                case "3_3" -> {
+                    String response = "CON Enter amount to withdraw";
+                    lastResponse = response;
+                    menuLevel = "4_3";
+                }
+                case "3_4" -> {
+                    String response = "CON Enter your account number";
+                    lastResponse = response;
+                    menuLevel = "4_4";
+                }
+            }
+        }
+        //Todo: Complete the logic for the rest of the menuLevel
+        return lastResponse;
     }
 }
